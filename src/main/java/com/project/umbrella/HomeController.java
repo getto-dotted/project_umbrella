83afae92ec45a0e05b89b575313e09fb57f3fb3d
getto <ejrl123@gmail.com>
2020-04-01 22:07:44 +0900

    로그인

--- src/main/java/com/project/umbrella/HomeController.java
+++ src/main/java/com/project/umbrella/HomeController.java
@@ -1,8 +1,5 @@
 package com.project.umbrella;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
